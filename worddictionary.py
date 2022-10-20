2a6c8a5958dbd8a8ee3a3c63c6ee56d4a6b4d5e5
Pablo A. Garcia <zer0@Pablos-MacBook-Pro.local>
2022-10-21 05:53:22 +0900

    library added with loop

--- worddictionary.py
+++ worddictionary.py
@@ -3,17 +3,27 @@
 # check if the word is within the dictionary
 # print that 
 
-def main():
-    word_dictionary = {
-        'hi': 'a way to greet someone',
-        'eyes': 'an organ for seeing and identifying objects or places',
-        'earth': 'a planet in our solar system'
-    }
-    while True:
-        word =input("Enter a word: ")
-        if word == "":
-            break
-        if word in word_dictionary:
-            print(word, ":", word_dictionary[word])
+from PyDictionary import PyDictionary
 
-main()
+dictionary = PyDictionary()
+
+while True:
+    word = input("Enter your word: ")
+    if word == "":
+        break 
+
+    print(dictionary.meaning(word))
+#def main():
+   # word_dictionary = {
+        #'hi': 'a way to greet someone',
+        #'eyes': 'an organ for seeing and identifying objects or places',
+        #'earth': 'a planet in our solar system'
+    #}
+    #while True:
+       # word =input("Enter a word: ")
+        #if word == "":
+            #break
+        #if word in word_dictionary:
+            #print(word, ":", word_dictionary[word])
+
+#main()
