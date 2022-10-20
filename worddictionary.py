d497a9a075a07345ebc656c5af19643b86ac992a
Pablo A. Garcia <zer0@Pablos-MacBook-Pro.local>
2022-10-21 05:44:56 +0900

    basic test code

---
+++ worddictionary.py
@@ -0,0 +1,19 @@
+# havea python dictionary that has a key/value pair that represents a word and it's definition
+# collect input from the user, input is a word 
+# check if the word is within the dictionary
+# print that 
+
+def main():
+    word_dictionary = {
+        'hi': 'a way to greet someone',
+        'eyes': 'an organ for seeing and identifying objects or places',
+        'earth': 'a planet in our solar system'
+    }
+    while True:
+        word =input("Enter a word: ")
+        if word == "":
+            break
+        if word in word_dictionary:
+            print(word, ":", word_dictionary[word])
+
+main()
